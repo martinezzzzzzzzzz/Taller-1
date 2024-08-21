2bb293e171d38ece9bd2d6f23022288690fbcf51
Emanuel <martinezvalencia1200@gmail.com>
2024-08-21 13:39:49 -0500

    Ejercicio 4

--- script.js
+++ script.js
@@ -32,3 +32,15 @@ else{
     console.log ("No tines descuento ")
 }
 
+//EJERCICIO 4 
+
+let numero1 = prompt ("Ingrese el primer número: ")
+let numero2 = prompt ("Ingrese el segundo número: ")
+if (numero1 < numero2) { 
+    console.log ("El primer número es mayor que el segundo ")
+
+}
+else if (numero1 < numero2) { 
+    console.log ("El segundo número es mayor que el segundo ")
+
+}
